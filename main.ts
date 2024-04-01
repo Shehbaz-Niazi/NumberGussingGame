ef35a350e4e3c41e71115c3a5743a46a08b15f8b
Shehbaz-Niazi <163367421+Shehbaz-Niazi@users.noreply.github.com>
2024-04-01 00:32:58 -0700

    NumberGussingGame

--- main.ts
+++ main.ts
@@ -1,71 +1,63 @@
 #! /usr/bin/env node
-
-import inquirer from "inquirer";
 import chalk from "chalk";
-import chalkAnimation from "chalk-animation";
+import showBanner from "node-banner";
+import inquirer from "inquirer";
 
-const sleep = () => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 3000);
-  });
-};
-
-async function getAnimation() {
-  let animation = chalkAnimation.karaoke("Are You Ready To Play This Game?");
-  await sleep();
-  animation.stop();
-}
-await getAnimation()
-
-let score: number = 0;
-
-async function getStarted() {
-  let decimal = Math.floor(Math.random() * 10);
-
-  let Tip;
-  if (decimal % 2 == 0) {
-    Tip = chalk.blue("Generated Number Is Even");
-  } else {
-    Tip = chalk.green("Generated Number Is ODD");
-  }
-
-  let askQuery = await inquirer.prompt([
-    {
-      name: "userGuess",
-      type: "number",
-      message: `Please enter Your Guess B/w 1 to 10 ${Tip}`,
-    },
-  ]);
-  console.log(
-    chalk.bgWhite.blue(
-      `Your Guess is ${askQuery.userGuess} And System Generated is ${decimal}`
-    )
+(async () => {
+  await showBanner(
+    "Number Gussing Game",
+    "Guess the number, challenge your wits! TypeScript fun awaits!",
+    "red",
+    "blue"
   );
-  if (askQuery.userGuess === decimal) {
-    score++;
-    console.log(
-      chalk.yellow.bold(
-        `Congratulation Your Your Guess is Correct. Your Score is ${score} `
-      )
-    );
-  } else {
-    console.log(
-      chalk.red.bold(
-        `Your Guess is incorrect. Better Luck Next Time your score id ${score} `
-      )
-    );
-  }
-}
+let score = 0
+let condition = true;
 
-async function start_loop() {
-  do {
-    await getStarted();
-    var again = await inquirer.prompt({
-      type: "list",
-      name: "restart",
-      message: "Do you want to restart ? Yes Or No ",
-      choices: ["yes", "no"],
-    });
-  } while (again.restart === "yes");
-}
-await start_loop();
+async function started(){
+    let generatdNumber = Math.floor(Math.random() * 10)
+    let Tip;
+    if(generatdNumber % 2==0){
+        console.log(chalk.blue.bold("\n                     -------------------------"));
+        console.log(chalk.bgBlue.white(`\t\t\tTip Number Is Even`));
+        console.log(chalk.blue.bold("                     -------------------------"));
+
+    }else{
+        console.log(chalk.green.bold("\n                     -------------------------"));
+        console.log(chalk.bgGreen.white(`\t\t\tTip Number Is odd`));
+        console.log(chalk.green.bold("                     -------------------------"));
+    }
+    let answer = await inquirer.prompt(
+        {
+            name: "num1",
+            type: "number",
+            message: chalk.yellow.bold("Enter Your Guess Number"),
+            validate: (input: number)=>{
+                if(isNaN(input)){
+                    return chalk.red.bold.italic ("Please Enter Numeric Number")
+                }
+                return true;
+            }
+        }
+        );
+        console.log(chalk.green.bold(`Your Guess Number Is ${answer.num1} And System Generated Number Is ${generatdNumber}`));
+        if(answer.num1 === generatdNumber){
+            score++
+            console.log(chalk.bgGreen.white.bold(`\n\tCongartulations Your Guess is Correct.. Your Score is ${score}`));
+        }else{
+            console.log(chalk.bgRed.white.bold(`\n\tWrong Guess!!... Try Again Your Score is ${score}\n`));   
+        }}
+    async function question (){
+        do{
+            await started()
+            var again = await inquirer.prompt(
+                {
+                    name: "loop",
+                    type: "list",
+                    message: "Do You Want To Continue Yes Or No",
+                    choices :["Yes", "No"]
+                }
+            )
+        }while(again.loop === "Yes")
+    };
+    await  question();
+})()
